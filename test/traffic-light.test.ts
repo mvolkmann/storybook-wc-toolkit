7a1dbbf1ab2fd0d6e63b435f8412ce1145463756
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-08-28 14:43:06 -0500

    progress

---
+++ test/traffic-light.test.ts
@@ -0,0 +1,15 @@
+/*
+ * This is a failed attempt to run a snapshot test in Storybook.
+
+import { expect, test } from "vitest";
+
+import { composeStories } from "@storybook/web-components";
+
+import * as stories from "../stories/traffic-light.stories";
+
+const { Default } = composeStories(stories);
+test("traffic-light snapshot", async () => {
+  await Default.run();
+  expect(document.body.firstChild).toMatchSnapshot();
+});
+*/
